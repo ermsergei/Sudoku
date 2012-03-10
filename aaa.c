3b30aa78a6f44f00666ca3e99ad603dc839ce067
Sergei Ermolaev <ermsergei@yahoo.com>
2012-03-09 19:34:15 -0500

    First commited

---
+++ aaa.c
@@ -0,0 +1,65 @@
+commit b0e21b43b1a1384f84688c1dd39ad18e99f6a041
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Mon Feb 20 12:58:18 2012 -0500
+
+    test file
+
+commit f85a911c602ee43c08f26609eb63fd7b99d2c77e
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Mon Feb 20 12:57:31 2012 -0500
+
+    test file
+
+commit 227769a03adcaa2f8be1bf71fe92ec55fc949a04
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Mon Feb 20 12:56:21 2012 -0500
+
+    test file
+
+commit 86c16d8f2f0c252e3b95b50b35023e70ad0597fb
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Mon Feb 20 12:46:10 2012 -0500
+
+    working version
+
+commit eff0eea5e0fa5796fe27b3c13b56e8559a536d09
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Mon Feb 20 12:34:00 2012 -0500
+
+    new
+
+commit ec997764f27c579fd8c3425f5f54edadd5571f96
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Mon Feb 20 12:31:53 2012 -0500
+
+    new
+
+commit 8997729a3323c0f1e7499985f4d6e6f219018261
+Author: Igor Mordkovitch <igor.mordkovitch@advantechwireless.com>
+Date:   Mon Feb 20 10:27:27 2012 -0500
+
+    Test
+
+commit a0675fb9677e7a5bdf641eac833153ce03092dfa
+Author: Igor Mordkovitch <igor.mordkovitch@advantechwireless.com>
+Date:   Mon Feb 20 10:22:41 2012 -0500
+
+    Testing github
+
+commit c209ebcc9a4ac44f8504ea2c9aa02f28ece46783
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Fri Feb 17 15:46:18 2012 -0500
+
+    Second file
+
+commit 459b74709614f88cc11c48bc3fd25980f14807de
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Thu Feb 16 13:26:38 2012 -0500
+
+    Sudoku code in java, recurtion part of SolveAll does not work properly
+
+commit b7f661dad4bc3b4923244301dd9a39fb78910019
+Author: Sergei Ermolaev <ermsergei@yahoo.com>
+Date:   Thu Feb 16 10:49:35 2012 -0500
+
+    This is my first try
